ba47e7ce827e1b8815ba9e2067336c688d37c43c
Ana Carolina de Araujo <ana.araujo11@hmail.com>
2016-10-01 15:14:30 -0300

    Add files via upload

---
+++ speak/login/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+  </head>
+
+  <body>
+    <form method="post" action="login.php" method="post">
+      <div><input type="text" name="user" placeholder="User" autofocus required></div>
+      <div><input type="password" name="pass" placeholder="Password" required></div>
+      <div><button type="submit">Login</button></div>
+    </form>
+    <br>
+    <p><a href="../new/">Register</a></p>
+  </body>
+</html>
---
+++ speak/valid.php
@@ -0,0 +1,3 @@
+<?php
+session_start();
+if (!$_SESSION['user']) { header("Location: login/"); }
